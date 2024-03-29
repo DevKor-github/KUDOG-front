ce5e9cd4f4bbd9193e3b437d8b5b58fb76b2e439
quburty <quburty@naver.com>
2024-03-29 12:45:53 +0900

    구독페이지 필터창 구현 및 디자인수정 완

---
+++ kudog/lib/pages/subscribe/ViewSubscribeFilterPage.dart
@@ -0,0 +1,214 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:kudog/pages/home/SetFilterPage.dart';
+
+class ViewSubscribeFilterPageWidget extends StatefulWidget {
+  const ViewSubscribeFilterPageWidget({Key? key}) : super(key: key);
+
+  @override
+  _ViewSubscribeFilterPageWidgetState createState() =>
+      _ViewSubscribeFilterPageWidgetState();
+}
+
+class _ViewSubscribeFilterPageWidgetState
+    extends State<ViewSubscribeFilterPageWidget> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  List<String> majors = ["전체", "정보대학", "공과대학", "디자인조형학부", "미디어학부", "경영대학"];
+  List<String> categories = [
+    "학부",
+    "대학원",
+    "교내 장학",
+    "교외 장학",
+    "근로 장학",
+    "학사 일정",
+    "학사자료실",
+    "자유게시판",
+    "공모전",
+    "채용정보",
+    "행사"
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Color(0x00ffffff),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_left),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          title: Text('구독 설정'),
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 21),
+          child: ListView(children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 21),
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '이름',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color(0xFFF4F2F2),
+                        enabledBorder:
+                            OutlineInputBorder(borderSide: BorderSide.none)),
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 21),
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '이메일',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color(0xFFF4F2F2),
+                        enabledBorder:
+                            OutlineInputBorder(borderSide: BorderSide.none)),
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+                  )
+                ],
+              ),
+            ),
+            Container(
+                child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(bottom: 16),
+                  child: Row(
+                    children: [
+                      Container(
+                          child: Text(
+                            '학과',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Color(0xFF1B1616),
+                              fontSize: 16,
+                              fontFamily: 'Pretendard',
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          margin: EdgeInsets.only(
+                            right: 12,
+                          )),
+                      Icon(Icons.edit_outlined)
+                    ],
+                  ),
+                ),
+                Row(
+                  children: [
+                    MajorCard(major: majors[0]),
+                    MajorCard(major: majors[1]),
+                    MajorCard(major: majors[2])
+                  ],
+                ),
+                Row(
+                  children: [
+                    MajorCard(major: majors[3]),
+                    MajorCard(major: majors[4]),
+                    MajorCard(major: majors[5])
+                  ],
+                )
+              ],
+            )),
+            Container(
+                margin: EdgeInsets.only(bottom: 26),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                            margin: EdgeInsets.only(top: 30, bottom: 10),
+                            child: Text(
+                              '카테고리',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Color(0xFF1B1616),
+                                fontSize: 16,
+                                fontFamily: 'Pretendard',
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ))
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Column(
+                          children: [
+                            CategoryCard(category: categories[0]),
+                            CategoryCard(category: categories[1]),
+                            CategoryCard(category: categories[2]),
+                            CategoryCard(category: categories[3]),
+                            CategoryCard(category: categories[4])
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            CategoryCard(category: categories[5]),
+                            CategoryCard(category: categories[6]),
+                            CategoryCard(category: categories[7]),
+                            CategoryCard(category: categories[8]),
+                            CategoryCard(category: categories[9])
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                )),
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8))),
+                  minimumSize: Size.fromHeight(40),
+                  backgroundColor: Color(0xffff0000)),
+              child: const Text(
+                '저장',
+                style: TextStyle(color: Color(0xffffffff)),
+              ),
+            )
+          ]),
+        ));
+  }
+}
--- kudog/lib/pages/subscribe/ViewSubscribePage.dart
+++ kudog/lib/pages/subscribe/ViewSubscribePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:kudog/pages/subscribe/ViewSubscribePageList.dart';
+import 'package:kudog/pages/subscribe/ViewSubscribeFilterPage.dart';
 
 class ViewSubscribePageWidget extends StatefulWidget {
   const ViewSubscribePageWidget({Key? key}) : super(key: key);
@@ -23,6 +25,144 @@ class _ViewSubscribePageWidgetState extends State<ViewSubscribePageWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 28,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '관심있는 소식만 빠르게 모아보세요',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                ),
+                TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                ViewSubscribeFilterPageWidget(),
+                          ));
+                    },
+                    child: Text(
+                      '편집',
+                      style: TextStyle(color: Color(0xFFFF4F59)),
+                    ))
+              ],
+            ),
+            const SizedBox(
+              height: 22,
+            ),
+            Expanded(
+                child: Column(children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ViewSubscribePageListWidget()));
+                },
+                child: Container(
+                  padding: EdgeInsets.all(16),
+                  decoration: const BoxDecoration(
+                      color: Color(0xffF4F2F2),
+                      borderRadius: BorderRadius.all(Radius.circular(9))),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.asset("assets/images/artboard.png"),
+                          Icon(
+                            Icons.settings_rounded,
+                            color: Color(0xFFCCC9C9),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "디조짱",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Flexible(
+                            child: Container(
+                                margin: EdgeInsets.only(right: 4),
+                                padding: EdgeInsets.fromLTRB(8, 2, 8, 2),
+                                height: 26,
+                                child: Text('컴퓨터학부',
+                                    style: TextStyle(
+                                        color: Color(0xFFFF3B47),
+                                        fontWeight: FontWeight.w500)),
+                                decoration: BoxDecoration(
+                                    border: Border.all(
+                                        width: 1, color: Color(0xFFFFD8DA)),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(6)),
+                                    color: Color(0xFFFFFFFF))),
+                          ),
+                          Flexible(
+                            child: Container(
+                                margin: EdgeInsets.only(right: 4),
+                                padding: EdgeInsets.fromLTRB(8, 2, 8, 2),
+                                height: 26,
+                                child: Text('디자인조형학부',
+                                    style: TextStyle(
+                                        color: Color(0xFFFF3B47),
+                                        fontWeight: FontWeight.w500)),
+                                decoration: BoxDecoration(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(6)),
+                                    color: Color(0x80FF3B47))),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                  margin: EdgeInsets.symmetric(vertical: 8),
+                  width: double.infinity,
+                  height: 40,
+                  child: OutlinedButton.icon(
+                      icon: const Icon(
+                        Icons.create_new_folder_outlined,
+                        size: 24,
+                      ),
+                      label: const Text(
+                        '구독함 추가',
+                        style: TextStyle(
+                            fontSize: 10, fontWeight: FontWeight.w500),
+                      ),
+                      onPressed: () => {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        ViewSubscribeFilterPageWidget()))
+                          },
+                      style: OutlinedButton.styleFrom(
+                        foregroundColor: Color(0xFF000000),
+                        shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(8))),
+                        minimumSize: Size.fromHeight(40),
+                      )))
+            ])),
+          ],
+        ),
+      ),
+    );
   }
 }
