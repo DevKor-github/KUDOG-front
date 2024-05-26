eaa4a9043469d9bc46b1dce4f477ff4dd36b144e
quburty <quburty@naver.com>
2024-05-26 15:54:12 +0900

    스크랩, 구독함 필터창에서 수정시 즉각반영

--- kudog/lib/pages/subscribe/ViewSubscribePageList.dart
+++ kudog/lib/pages/subscribe/ViewSubscribePageList.dart
@@ -16,7 +16,7 @@ class ViewSubscribePageListWidget extends StatefulWidget {
       {Key? key, required this.subscribeList, required this.boxId, this.date})
       : super(key: key);
 
-  final List<Subscribe> subscribeList;
+  List<Subscribe> subscribeList;
   int boxId;
 
   DateTime? date = DateTime.now();
@@ -42,60 +42,6 @@ class _ViewSubscribePageListWidgetState
     });
   }
 
-  bool isMoreRequesting = false;
-
-  // 드레그 거리를 체크하기 위함
-  // 해당 값을 평균내서 50%이상 움직였을때 데이터 불러오는 작업을 하게됨.
-  double _dragDistance = 0;
-
-  scrollNotification(notification) {
-    // 스크롤 최대 범위
-    var containerExtent = notification.metrics.viewportDimension;
-
-    if (notification is ScrollStartNotification) {
-      // 스크롤을 시작하면 발생(손가락으로 리스트를 누르고 움직이려고 할때)
-      // 스크롤 거리값을 0으로 초기화함
-      _dragDistance = 0;
-    } else if (notification is OverscrollNotification) {
-      // 안드로이드에서 동작
-      // 스크롤을 시작후 움직일때 발생(손가락으로 리스트를 누르고 움직이고 있을때 계속 발생)
-      // 스크롤 움직인 만큼 빼준다.(notification.overscroll)
-      _dragDistance -= notification.overscroll;
-    } else if (notification is ScrollUpdateNotification) {
-      // ios에서 동작
-      // 스크롤을 시작후 움직일때 발생(손가락으로 리스트를 누르고 움직이고 있을때 계속 발생)
-      // 스크롤 움직인 만큼 빼준다.(notification.scrollDelta)
-      _dragDistance -= notification.scrollDelta!;
-    } else if (notification is ScrollEndNotification) {
-      // 스크롤이 끝났을때 발생(손가락을 리스트에서 움직이다가 뗐을때 발생)
-
-      // 지금까지 움직인 거리를 최대 거리로 나눈다.
-      var percent = _dragDistance / (containerExtent);
-      // 해당 값이 -0.4(40프로 이상) 아래서 위로 움직였다면
-      if (percent <= -0.4) {
-        // maxScrollExtent는 리스트 가장 아래 위치 값
-        // pixels는 현재 위치 값
-        // 두 같이 같다면(스크롤이 가장 아래에 있다)
-        if (notification.metrics.maxScrollExtent ==
-            notification.metrics.pixels) {
-          setState(() {
-            // 서버에서 데이터를 더 가져오는 효과를 주기 위함
-            // 하단에 프로그레스 서클 표시용
-            isMoreRequesting = true;
-          });
-
-          // 서버에서 데이터 가져온다.
-          requestMore().then((value) {
-            setState(() {
-              // 다 가져오면 하단 표시 서클 제거
-              isMoreRequesting = false;
-            });
-          });
-        }
-      }
-    }
-  }
-
   @override
   void initState() {
     super.initState();
@@ -118,6 +64,15 @@ class _ViewSubscribePageListWidgetState
     });
   }
 
+  void _refreshSubscribes() async {
+    await Provider.of<NoticeService>(context, listen: false).getSubscribes();
+
+    setState(() {
+      widget.subscribeList =
+          Provider.of<NoticeService>(context, listen: false).subscribeList;
+    });
+  }
+
   @override
   void dispose() {
     dio.close();
@@ -130,14 +85,6 @@ class _ViewSubscribePageListWidgetState
     });
     Provider.of<NoticeService>(context, listen: false)
         .getSubscribedNotices(subscribe!.id, widget.date!);
-  }
-
-  Future<void> requestMore() async {
-    // setState(() {
-    //   currentPage++;
-    // });
-    // Provider.of<NoticeService>(context, listen: false)
-    //     .addSubscribedNotices(currentPage);
   }
 
   @override
@@ -165,6 +112,7 @@ class _ViewSubscribePageListWidgetState
             children: [
               Row(mainAxisSize: MainAxisSize.min, children: [
                 DropdownMenu(
+                  key: GlobalKey(),
                   initialSelection: widget.boxId,
                   onSelected: (value) {
                     if (value == null) return;
@@ -192,7 +140,7 @@ class _ViewSubscribePageListWidgetState
                                   builder: (context) =>
                                       ViewSubscribeFilterPageWidget(
                                         subscribe: subscribe,
-                                      )))
+                                      ))).then((value) => _refreshSubscribes())
                         },
                     icon: Icon(Icons.settings_rounded))
               ]),
@@ -258,17 +206,6 @@ class _ViewSubscribePageListWidgetState
                 height: 22,
               ),
               Expanded(
-                  child: NotificationListener<ScrollNotification>(
-                onNotification: (ScrollNotification notification) {
-                  /*
-                     스크롤 할때 발생되는 이벤트
-                     해당 함수에서 어느 방향으로 스크롤을 했는지를 판단해
-                     리스트 가장 밑에서 아래서 위로 40프로 이상 스크롤 했을때 
-                     서버에서 데이터를 추가로 가져오는 루틴이 포함됨.
-                    */
-                  scrollNotification(notification);
-                  return false;
-                },
                 child: ListView.builder(
                   physics: AlwaysScrollableScrollPhysics(),
                   padding: EdgeInsets.zero,
@@ -282,14 +219,7 @@ class _ViewSubscribePageListWidgetState
                     );
                   },
                 ),
-              )),
-              Container(
-                height: isMoreRequesting ? 50.0 : 0,
-                color: Colors.white,
-                child: Center(
-                  child: CircularProgressIndicator(),
-                ),
-              ),
+              )
             ],
           ),
         ),
